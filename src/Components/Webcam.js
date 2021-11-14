c3aaf8cce6cce65869deee194d4ca677e1cb23c0
Vishal Kumar Vasnani <vishalvasnani123@gmail.com>
2021-11-13 21:42:59 -0800

    connect webcam stream to BE

--- src/Components/Webcam.js
+++ src/Components/Webcam.js
@@ -1,15 +1,42 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import Webcam from 'react-webcam';
+import https from 'https';
+
+const dataURLtoFile = (dataurl, filename) => {
+  const arr = dataurl.split(',');
+  const mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+  while (n) {
+    u8arr[n - 1] = bstr.charCodeAt(n - 1);
+    n -= 1;
+  }
+  return new File([u8arr], filename, { type: mime });
+};
 
 const WebcamComponent = () => {
   const [startCapturing, setStartCapturing] = useState();
-  const [imageSrc, setImageSrc] = useState();
   const webcamRef = React.useRef(null);
 
   const capture = React.useCallback(
-    () => {
+    async () => {
       const imgSrc = webcamRef.current.getScreenshot();
-      setImageSrc(imgSrc);
+      const file = dataURLtoFile(imgSrc);
+      const formData = new FormData();
+      formData.append('file', file);
+      const options = {
+        url: 'upload_image',
+        method: 'POST',
+        data: formData,
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      };
+      const result = await axios(options);
+      console.log(result.data);
     },
     [webcamRef],
   );
@@ -24,23 +51,20 @@ const WebcamComponent = () => {
     setStartCapturing();
   };
 
-  if (startCapturing !== undefined) {
-    console.log(imageSrc);
-  }
-
   return (
     <div>
       <Webcam
         audio={false}
-        height={600}
+        height={400}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
-        width={600}
+        width={400}
         screenshotQuality={1}
         videoConstraints={{
-          width: 600,
-          height: 600,
+          width: 400,
+          height: 400,
           facingMode: { exact: 'environment' },
+          // facingMode: 'user',
         }}
       />
       {(!startCapturing) && (<button type="submit" onClick={() => handleStartCapturing()}>Start Capturing</button>)}
